56feb7aab3ca2431a2b2b03e3af1cc3bd5e4dfb9
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-11-03 19:21:05 +0100

    finetune

--- finetune.py
+++ finetune.py
@@ -89,7 +89,7 @@ def objective(trial: optuna.trial.Trial):
         accelerator=device,
         devices="auto",
         logger=logger,
-        callbacks=[optuna.integration.PyTorchLightningPruningCallback(trial, monitor='val_acc_node', mode='max')]
+        callbacks=[optuna.integration.PyTorchLightningPruningCallback(trial, monitor='val_acc_node')]
     )
     hyperparameters = dict(stroke_emb_nb=stroke_emb_nb, rel_emb_nb=rel_emb_nb, batch_size=batch_size, lr=lr, lambda1=lambda1, lambda2=lambda2, dropout=dropout, node_gat_input_size=node_gat_input_size, edge_gat_input_size=edge_gat_input_size, node_gat_hidden_size=node_gat_hidden_size, edge_gat_hidden_size=edge_gat_hidden_size, node_gat_output_size=node_gat_output_size, edge_gat_output_size=edge_gat_output_size, gat_n_heads=gat_n_heads)
     trainer.logger.log_hyperparams(hyperparameters)
