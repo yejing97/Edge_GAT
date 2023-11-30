0bae9355b7a625de5fef6ca8c493ab138bcd681a
E19B516G <yejing.xie@etu.univ-nantes.fr>
2023-11-30 11:11:59 +0100

    changes

--- finetune.py
+++ finetune.py
@@ -26,12 +26,12 @@ def objective(trial: optuna.trial.Trial):
     stroke_emb_nb = trial.suggest_categorical('stroke_emb_nb', [150])
     # rel_emb_nb = trial.suggest_int('rel_emb_nb', 5, 11, step=5)
     rel_emb_nb = trial.suggest_categorical('rel_emb_nb', [10])
-    total_batch_size = trial.suggest_categorical('total_batch_size', [128, 192, 256, 320, 384, 448, 512])
+    total_batch_size = trial.suggest_categorical('total_batch_size', [128, 192, 256, 320, 384])
     # batch_size = trial.suggest_categorical('batch_size', [16, 32, 64])
     max_node = trial.suggest_categorical('max_node', [4, 6, 8, 10, 12, 16])
     batch_size = total_batch_size // max_node
     lr = trial.suggest_float('lr', 1e-6, 1e-1, log=True)
-    lambda1 = trial.suggest_float('lambda1', 0, 1, step=0.1)
+    lambda1 = trial.suggest_float('lambda1', 0.4, 1, step=0.1)
     lambda2 = 1 - lambda1
     dropout = trial.suggest_float('dropout', 0.2, 0.6, step=0.1)
     gat_n_heads = trial.suggest_categorical('gat_n_heads', [1, 2, 4, 8])
