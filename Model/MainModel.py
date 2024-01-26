ae823cb631635730177aa3e65349639971f97671
E19B516G <yejing.xie@etu.univ-nantes.fr>
2024-01-26 17:15:50 +0100

    fff

--- Model/MainModel.py
+++ Model/MainModel.py
@@ -133,7 +133,7 @@ class MainModel(pl.LightningModule):
             # self.softmax = torch.nn.Softmax(dim=-1)
         else:
             # self.node_emb = XceptionTime(2, node_gat_parm[0])
-            self.node_emb = torch.nn.LSTM(2, int(node_gat_parm[0]/2),10, bidirectional = True)
+            self.node_emb = torch.nn.LSTM(2, int(node_gat_parm[0]/2),2, bidirectional = True)
             # self.node_emb = TransformerModel(2, node_gat_parm[0])
             self.edge_emb = Edge_emb(edge_emb_parm, dropout)
             # for param in self.edge_emb.parameters():
